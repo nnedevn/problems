7541f8dcca636bec5b7b7ff327c02e7a63917821
Nikolay Nedev <nedevn@gmail.com>
2018-08-20 22:20:33 -0700

    Adds Reverser a stack with one other stack and a single temp variable

--- problems/Kal Academy/Stack.js
+++ problems/Kal Academy/Stack.js
@@ -12,50 +12,70 @@
 //   Delete that element from storage
 //   Return saved element
 // Create size method on Stack prototype
-//   Return count 
+//   Return count
 
 // Create a Stack
-let Stack = function(){
+let Stack = function() {
   this.count = 0;
-  this.storage = {}
-}
+  this.storage = {};
+};
 
 //Adds a value to the end(top) of the stack
-Stack.prototype.push = function(value){
+Stack.prototype.push = function(value) {
   this.storage[this.count] = value;
-  this.count ++;
-}
+  this.count++;
+};
 
 // Removes and returns the value at the end of the stack;
 
-Stack.prototype.pop = function(){
+Stack.prototype.pop = function() {
   // check to see if the stack is empty
   if (this.count === 0) {
     return undefined;
   }
 
-  this.count --;
+  this.count--;
   let result = this.storage[this.count];
   delete this.storage[this.count];
-  
+
   return result;
-}
+};
 
-Stack.prototype.peek = function(){
+Stack.prototype.peek = function() {
   return this.storage[this.count - 1];
-}
+};
 
-Stack.prototype.size = function(){
+Stack.prototype.size = function() {
   return this.count;
+};
+
+// Reverse a stack using another stack and a single temp varaiable
+let st1 = new Stack();
+let st2 = new Stack();
+let temp = null;
+
+for (let i = 0; i < 30; i++) {
+  st1.push(i);
 }
 
-let myStack = new Stack;
+for (let i = 0; i < st1.size(); i++) {
+  temp = st1.pop();
 
-myStack.push('lalal');
-myStack.push("something else");
-myStack.push('the last one');
-// console.log(myStack);
-// console.log(myStack.peek());
-// console.log(myStack.pop());
-// console.log(myStack.peek());
- console.log(myStack.size());
+  count = st1.size() - i;
+  while (count > 0) {
+    st2.push(st1.pop());
+    count--;
+  }
+
+  st1.push(temp);
+
+  count = st2.size();
+  while (count > 0) {
+    st1.push(st2.pop());
+    count--;
+  }
+}
+
+console.log("stack2 =", st2);
+console.log("stack1 =", st1);
+console.log("stack1 size =", st1.size());
