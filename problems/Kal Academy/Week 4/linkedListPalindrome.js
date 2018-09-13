28bb746304f0c9a480cd8fecd614b95df31f8de0
Nick Nedev <nedevn@gmail.com>
2018-09-13 15:28:44 -0700

    Adds linkedListPalindrome.js

--- problems/Kal Academy/Week 4/linkedListPalindrome.js
+++ problems/Kal Academy/Week 4/linkedListPalindrome.js
@@ -1,4 +1,10 @@
 //Write an algorithm to determine if a linkedlist is a palindrome
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
 class LinkedList {
   constructor() {
@@ -7,15 +13,65 @@ class LinkedList {
     this.length = 0;
   }
 
-  push(value) {}
+  push(value) {
+    const node = new Node(value);
+    this.length++;
+    if (!this.head) {
+      this.head = node;
+    } else {
+      this.tail.next = node;
+    }
+    this.tail = node;
+  }
 
-  pop() {}
+  pop() {
+    return this.delete(this.length - 1);
+  }
 
-  _find(value, test = this._test) {}
+  _find(value, test = this._test) {
+    let current = this.head;
+    let i = 0;
+    while (current) {
+      if (test(value, current.value, i, current)) {
+        return current;
+      }
+      current = current.next;
+      i++;
+    }
+    return null;
+  }
 
-  get(index) {}
+  get(index) {
+    const node = this._find(index, this._testIndex);
+    if (!node) {
+      return null;
+    } else {
+      return node.value;
+    }
+  }
 
-  delete(index) {}
+  delete(index) {
+    if (index === 0) {
+      const head = this.head;
+      if (head) {
+        this.head = head.next;
+      } else {
+        this.head = null;
+        this.tail = null;
+      }
+      this.length--;
+      return head.value;
+    }
+    const node = this.find(index - 1, this._testIndex);
+    const excise = node.next;
+    if (!excise) return null;
+    node.next = excise.next;
+    if (node.next && !node.next.next) {
+      this.tail = node.next;
+    }
+    this.length--;
+    return excise.value;
+  }
 
   _testIndex(search, __, i) {
     return search === i;
@@ -26,9 +82,30 @@ class LinkedList {
   }
 }
 
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
+//-------
+
+let mylist = new LinkedList;
+let arr = ['r', 'a','c','e','c','a','r'];
+arr.forEach(element => {
+  mylist.push(element);
+});
+
+
+function linkedListPalindrome(linkedList){
+  let tempStack = [];
+
+  for (let i = 0; i < linkedList.length; i++ ){
+    tempStack.push( linkedList.get(i));
   }
+
+  for (let i = 0; i < linkedList.length; i++){
+    if (linkedList.get(i) !== tempStack.pop()){
+      return false;
+    }
+    return true;
+  }
+
+  console.log(tempStack);
 }
+
+console.log(linkedListPalindrome(mylist));
