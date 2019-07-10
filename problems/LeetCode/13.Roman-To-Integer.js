25969aa8cc0c58f8a5bbd2886360a7b47268513d
Nick Nedev <nick@nicknedev.com>
2019-07-10 01:16:32 -0700

    Adds Roman-To-Integer.js simplified solution.

--- problems/LeetCode/13.Roman-To-Integer.js
+++ problems/LeetCode/13.Roman-To-Integer.js
@@ -1,34 +1,72 @@
 /**
- * @param {string} s
- * @return {number}
+* Given an string in roman no format (s)  your task is to convert it to integer.
  */
-var romanToInt = function(s) {
+/**
+ * 
+ * @param {String} roman
+ * @return {int}
+ */
+
+function romanToInt(roman) {
+  let romanArr = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I",
+  ];
+  let intArr = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  let position = 0
   let result = 0;
 
-  for (let i = 0; i < s.length; i++){
-    switch (s[i]){
-      case 'M':
-        result += 1000;
-        break;
-      case 'D':
-        result += 500;
-        break;
-      case 'C':
-        result += 100;
-        break;
-      case 'L':
-        result += 50;
-        break;
-      case 'X':
-        result += 10;
-        break;
-      case 'V':
-        result += 5;
-        break;
-      case 'I':
-        result += 1;
+  for (let i = 0; i < romanArr.length; i++) {
+    if (roman.substring(position, position + romanArr[i].length) === romanArr[i]) {
+      do {
+        result += intArr[i];
+        position += romanArr[i].length
+      } while (roman.substring(position, position + romanArr[i].length) === romanArr[i])
     }
   }
   return result;
-};
+}
 
+/**
+ * 
+ * @param {String} roman
+ * @return {int} 
+ */
+function romatToIntSimplified(romanStr) {
+  if (romanStr.length === 0 || romanStr === null) { return }
+
+  let romanMap = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+  }
+
+  let result = romanMap[romanStr[romanStr.length - 1]];
+
+  for (let i = romanStr.length - 2; i >= 0; i--) {
+    if (romanMap[romanStr[i]] >= romanMap[romanStr[i + 1]]) {
+      result += romanMap[romanStr[i]];
+    } else {
+      result -= romanMap[romanStr[i]];
+    }
+  }
+
+  return result;
+}
+
+console.log(romatToIntSimplified("MCMV"));
